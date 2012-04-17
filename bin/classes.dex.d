72efffaa4e6aefe9915b44a77bfd1c2d5c245967
Nicholas Nuing <nicholas.nuing@gmail.com>
2012-04-18 03:29:05 +0800

    Completed the Hello Android tutorial.

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -2,6 +2,7 @@
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/BuildConfig.class \
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R$attr.class \
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R$drawable.class \
+/Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R$id.class \
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R$layout.class \
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R$string.class \
 /Users/nicholas/src/android-dev/TerminalAndroid/bin/classes/com/example/terminalandroid/R.class \
--- gen/R.java.d
+++ gen/R.java.d
@@ -4,4 +4,7 @@
 /Users/nicholas/src/android-dev/TerminalAndroid/res/drawable-mdpi/ic_launcher.png \
 /Users/nicholas/src/android-dev/TerminalAndroid/res/layout/main.xml \
 /Users/nicholas/src/android-dev/TerminalAndroid/res/values/strings.xml \
+/Users/nicholas/src/android-dev/TerminalAndroid/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/nicholas/src/android-dev/TerminalAndroid/bin/res/drawable-ldpi/ic_launcher.png \
+/Users/nicholas/src/android-dev/TerminalAndroid/bin/res/drawable-mdpi/ic_launcher.png \
 /Users/nicholas/src/android-dev/TerminalAndroid/AndroidManifest.xml \
